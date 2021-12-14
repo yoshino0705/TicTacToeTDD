96fb1e12b0b48cf9d471a4862fb00bc9e131436b
yoshino0705 <chen.wei-hao@hotmail.com>
2021-12-14 17:04:03 -0500

    test(Board): should render Xs or Os

--- src/components/Board/Board.react.test.tsx
+++ src/components/Board/Board.react.test.tsx
@@ -1,15 +1,22 @@
-import {ReactWrapper, mount} from 'enzyme'
+import {mount} from 'enzyme'
 import {Board} from './Board'
 
 describe('Board', () => {
-  let wrapper: ReactWrapper
-
-  beforeEach(() => {
-    wrapper = mount(<Board />)
+  it('should render 9 grids', () => {
+    const wrapper = mount(<Board />)
+    const components = wrapper.find('[data-testid="game-grid"]')
+    expect(components).toHaveLength(9)
   })
 
-  it('should render 9 grids', () => {
-    const components = wrapper.find('[data-testid="game-grid"]')
+  it('should render 9 Xs', () => {
+    const wrapper = mount(<Board />)
+    const components = wrapper.find('[value="X"]')
+    expect(components).toHaveLength(9)
+  })
+
+  it('should render 9 Os', () => {
+    const wrapper = mount(<Board />)
+    const components = wrapper.find('[value="O"]')
     expect(components).toHaveLength(9)
   })
 })
