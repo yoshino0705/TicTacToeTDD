2dab576e0272e96afd05820d91f10f538bb89ed0
yoshino0705 <chen.wei-hao@hotmail.com>
2021-12-14 15:14:25 -0500

    fix(Board): should render 9 game grids to pass test

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,5 +1,10 @@
 import {FC} from 'react'
 
 export const Board: FC = () => {
-  return <div data-testid="game-board"></div>
+  const grids = []
+  for (let i = 0; i < 9; i++) {
+    grids.push(<div key={i} data-testid="game-grid"></div>)
+  }
+
+  return <div data-testid="game-board">{grids}</div>
 }
