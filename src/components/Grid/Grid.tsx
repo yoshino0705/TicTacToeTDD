f0a89cbc451b7722e0c816fcde9c13dd6417801d
yoshino0705 <chen.wei-hao@hotmail.com>
2021-12-14 19:16:56 -0500

    fix(Grid): should be clickable

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -1,13 +1,16 @@
-import {FC} from 'react'
+import {FC, MouseEventHandler} from 'react'
 
 interface GridProps {
-  value: string
+  value?: string
+  onClick: MouseEventHandler
 }
 
-export const Grid: FC<GridProps> = ({value}) => {
+export const Grid: FC<GridProps> = ({value, onClick}) => {
   return value === 'X' || value === 'O' ? (
-    <div data-testid="game-grid">{value}</div>
+    <div data-testid="game-grid" onClick={onClick}>
+      {value}
+    </div>
   ) : (
-    <div data-testid="game-grid"></div>
+    <div data-testid="game-grid" onClick={onClick}></div>
   )
 }
