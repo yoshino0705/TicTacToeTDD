aff23fc56e379e31b2195dcc0b09654d5a7a2174
yoshino0705 <chen.wei-hao@hotmail.com>
2021-12-14 21:27:47 -0500

    fix(Board): pass onClick to Grids, and remove unnecessary tests

--- src/components/Board/Board.react.test.tsx
+++ src/components/Board/Board.react.test.tsx
@@ -1,44 +1,11 @@
-import {ReactWrapper, mount} from 'enzyme'
-import React from 'react'
+import {mount} from 'enzyme'
 
 import {Board} from './Board'
-import {Step} from '../../utils/types'
-import {TOTAL_GRIDS} from '../../utils/constants'
 
 describe('Board', () => {
   it('should render 9 grids', () => {
     const wrapper = mount(<Board />)
     const components = wrapper.find('[data-testid="game-grid"]')
-    expect(components).toHaveLength(9)
-  })
-
-  it('should render 9 Xs if initial state is all Xs', () => {
-    const mockSteps: Step[] = []
-    for (let i = 0; i < TOTAL_GRIDS; i++) {
-      mockSteps.push({
-        symbol: 'X',
-        position: i,
-      })
-    }
-
-    React.useState = jest.fn().mockReturnValue([mockSteps, {}])
-    const wrapper = mount(<Board />)
-    const components = wrapper.find('[value="X"]')
-    expect(components).toHaveLength(9)
-  })
-
-  it('should render 9 Os if initial state is all Os', () => {
-    const mockSteps: Step[] = []
-    for (let i = 0; i < TOTAL_GRIDS; i++) {
-      mockSteps.push({
-        symbol: 'O',
-        position: i,
-      })
-    }
-
-    React.useState = jest.fn().mockReturnValue([mockSteps, {}])
-    const wrapper = mount(<Board />)
-    const components = wrapper.find('[value="O"]')
     expect(components).toHaveLength(9)
   })
 
--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -3,18 +3,26 @@ import {FC, useState} from 'react'
 import {Grid} from '../Grid'
 import {Step} from '../../utils/types'
 
-import {getGrids} from '../../utils/helper'
+import {getGrids, getTurn} from '../../utils/helper'
 
-interface BoardProps {}
-
-export const Board: FC<BoardProps> = () => {
+export const Board: FC = () => {
   const [steps, setSteps] = useState<Step[]>([])
   const grids = getGrids({steps})
 
   return (
     <div data-testid="game-board">
       {grids.map((symbol, i) => (
-        <Grid key={i} value={symbol} />
+        <Grid
+          key={i}
+          value={symbol}
+          onClick={() => {
+            const newStep: Step = {
+              symbol: getTurn({steps}),
+              position: i,
+            }
+            setSteps([...steps, newStep])
+          }}
+        />
       ))}
     </div>
   )
