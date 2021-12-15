2779e1d4cdf22a5d969d7f8398975280b41d0dae
yoshino0705 <chen.wei-hao@hotmail.com>
2021-12-14 22:54:35 -0500

    fix(Game): should show X's turn

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,16 +1,18 @@
 import {FC, useState} from 'react'
 
 import {Step} from '../../utils/types'
+import {getTurn} from '../../utils/helper'
 
 import {Announcer} from '../Announcer'
 import {Board} from '../Board'
 
 export const Game: FC = () => {
   const [steps, setSteps] = useState<Step[]>([])
+  const turn = getTurn({steps})
 
   return (
     <>
-      <Announcer text="" />
+      <Announcer text={`Now It's ${turn}'s Turn!`} />
       <Board steps={steps} setSteps={setSteps} />
     </>
   )
