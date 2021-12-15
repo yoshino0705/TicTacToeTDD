5748cbe28cb3a77793b2596326cf8b19840011ce
yoshino0705 <chen.wei-hao@hotmail.com>
2021-12-15 01:24:26 -0500

    test(ActionContainer): should render Rewind button

--- src/components/ActionContainer/ActionContainer.react.test.tsx
+++ src/components/ActionContainer/ActionContainer.react.test.tsx
@@ -3,7 +3,16 @@ import {ActionContainer} from './ActionContainer'
 
 describe('ActionContainer', () => {
   it('should render Reset button', () => {
-    const wrapper: ReactWrapper = mount(<ActionContainer />)
+    const wrapper: ReactWrapper = mount(
+      <ActionContainer onReset={jest.fn()} onRewind={jest.fn()} />
+    )
     expect(wrapper.text()).toBe('Reset')
+  })
+
+  it('should render Rewind button', () => {
+    const wrapper: ReactWrapper = mount(
+      <ActionContainer onReset={jest.fn()} onRewind={jest.fn()} />
+    )
+    expect(wrapper.text()).toBe('Rewind')
   })
 })
