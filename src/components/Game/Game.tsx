04425a7b4997ab86818d05ebee3eb28bf35d84ec
yoshino0705 <chen.wei-hao@hotmail.com>
2021-12-15 00:18:30 -0500

    fix(Game): should show X wins

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -2,7 +2,9 @@ import {FC, useState} from 'react'
 import classNames from 'classnames'
 
 import {Step} from '../../utils/types'
-import {getTurn} from '../../utils/helper'
+import {GAME_RESULT} from '../../utils/constants'
+
+import {getTurn, getGameResult} from '../../utils/helper'
 
 import {Announcer} from '../Announcer'
 import {Board} from '../Board'
@@ -11,14 +13,22 @@ import './Game.css'
 export const Game: FC = () => {
   const [steps, setSteps] = useState<Step[]>([])
   const turn = getTurn({steps})
+  const gameResult = getGameResult({steps})
 
   return (
     <div className={classNames('game')}>
       <Announcer
         text={
-          <span>
-            Now It's <span className={classNames('turn')}>{turn}</span>'s Turn!
-          </span>
+          gameResult === GAME_RESULT.xWin ? (
+            <span>
+              <span className={classNames('turn')}>X</span> Wins!
+            </span>
+          ) : (
+            <span>
+              Now It's <span className={classNames('turn')}>{turn}</span>'s
+              Turn!
+            </span>
+          )
         }
       />
       <Board steps={steps} setSteps={setSteps} />
