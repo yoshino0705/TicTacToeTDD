0ce986106c5d4cfa6ea3b3e22d8f0502ca54dc50
yoshino0705 <chen.wei-hao@hotmail.com>
2021-12-15 00:31:08 -0500

    test(Announcer): fix to comply with type definition of text

--- src/components/Announcer/Announcer.react.test.tsx
+++ src/components/Announcer/Announcer.react.test.tsx
@@ -3,7 +3,9 @@ import {Announcer} from './Announcer'
 
 describe('Announcer', () => {
   it('should render text', () => {
-    const wrapper: ShallowWrapper = shallow(<Announcer text="Test" />)
+    const wrapper: ShallowWrapper = shallow(
+      <Announcer text={<span>Test</span>} />
+    )
     expect(wrapper.text()).toBe('Test')
   })
 })
